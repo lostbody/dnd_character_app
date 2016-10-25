1486a62f2c17f2e9771e0e4383e3f1aac3dde8b3
lostbody <g.voumvoulaki@gmail.com>
2016-10-25 18:37:03 +0300

    update create_dungeon with classes etc

--- create_dungeon.py
+++ create_dungeon.py
@@ -1,41 +1,105 @@
-
 class Position(object):
 
     def __init__(self, row, col):
-        Position.row = row
-        Position.col = col
+        self.row = row
+        self.col = col
+
+    def __str__(self):
+        return str(self.row) + " , " + str(self.col)
+
+    def south(self):
+        return Position(self.row+1, self.col)
+
+    def go_south(self):
+        self.row += 1
+        return self
+
+    def north(self):
+        return Position(self.row-1, self.col)
+
+    def go_north(self):
+        self.row -= 1
+        return self
+
+    def east(self):
+        return Position(self.row, self.col+1)
+
+    def go_east(self):
+        self.col += 1
+        return self
+
+    def west(self):
+        return Position(self.row, self.col-1)
+
+    def go_west(self):
+        self.col -= 1
+        return self
+
+class Player(object):
+
+    def __init__(self, position):
+        self.position = Position(position.row, position.col)
 
 
-class Dungeon:
+class Dungeon(object):
 
     def __init__(self, max_size):
         self.max_size = max_size
-        self.entrance = Position(0, max_size)
+        self.entrance = Position(0, 0)
         self.boss = Position(max_size, 2)
-        self.current = Position(2, 2)
 
-    def south(self):
-        self.current.col -= 1
+    def get_directions(self, position):
 
-    def north(self):
-        self.current.col += 1
-
-    def east(self):
-        self.current.row += 1
-
-    def west(self):
-        self.current.row -= 1
-
-    def get_directions(self, current):
+        global options
 
         options = ['north', 'south', 'west', 'east']
 
-        if self.current.col == 0:
+        if position.col == 0:
             options.remove('west')
-        if self.current.col == self.max_size:
+        if position.col == self.max_size:
             options.remove('east')
-        if self.current.row == 0:
+        if position.row == 0:
             options.remove('north')
-        if self.current.row == self.max_size:
+        if position.row == self.max_size:
             options.remove('south')
         return options
+
+
+def enter_dungeon():
+    print "You enter the dungeon!"
+    global dungeon, position, player
+    dungeon = Dungeon(4)
+    position = dungeon.entrance
+    player = Player(position)
+    while True:
+        print "\nWhere do you want to go? Choose direction or press exit to leave the dungeon\n"
+        print dungeon.get_directions(player.position)
+
+        print "you are at position: ", player.position
+
+        chosen_move = raw_input(">")
+
+        if chosen_move in options:
+            if chosen_move == "west":
+                player.position.go_west()
+            elif chosen_move == "east":
+                player.position.go_east()
+            elif chosen_move == "north":
+                player.position.go_north()
+            elif chosen_move == "south":
+                player.position.go_south()
+
+        elif chosen_move == "exit":
+            return
+
+        else:
+            print "Sorry you can't go that way"
+            continue
+
+        # print player.position
+
+
+enter_dungeon()
+# print player.position
+# position1.go_east()
+# print position1
